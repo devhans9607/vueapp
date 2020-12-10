225ef96003c3899273a82faad5d11fe0aa7b293e
devhans9607 <crazyun2005@gmail.com>
2020-12-10 19:33:05 +0900

    successfully retrieved posts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div>
+    <User />
+  </div>
 </template>
 
 <style scoped>
@@ -7,3 +9,13 @@ h1 {
   color: navy;
 }
 </style>
+
+<script>
+import User from "./components/User.vue";
+
+export default {
+  components: {
+    User
+  }
+};
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-for="(post, idx) in posts" :key="idx">
+      <h2>{{post.postId}}</h2>
+      <h2>{{post.title}}</h2>
+<!--      <img :src="`${user.avatar}`" alt />-->
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      posts: null
+    };
+  },
+  methods: {
+    fetchData() {
+      axios
+          .get("http://localhost:8080/api/v1/posts")
+          .then(res => {
+            console.log("받았다고~!!");
+            console.log(res.data.content.simplePostPage.content);
+            this.posts = res.data.content.simplePostPage.content;
+            //this.totalPage = res.data.total_pages;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+    }
+  },
+  created() {
+    this.fetchData();
+  }
+};
+</script>
